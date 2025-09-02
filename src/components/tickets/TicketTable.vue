c15ed74b34c3c2c6f8d6043505569a6c5cf7229a
Rayandson <rayandson_silva@hotmail.com>
2025-09-01 22:25:18 -0300

    feat: adjust pagination ui

--- src/components/tickets/TicketTable.vue
+++ src/components/tickets/TicketTable.vue
@@ -394,7 +394,7 @@
     </div>
     <div
       v-if="pagination"
-      class="flex items-center justify-between px-6 py-4 border-t border-gray-200 dark:border-gray-700"
+      class="mt-5 flex items-center justify-between px-6 py-1"
     >
       <span class="text-sm text-gray-600 dark:text-gray-400"
         >Página {{ currentPage }} de {{ totalPages }}</span
